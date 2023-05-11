c68cf7eae92a4688c8cbd6c806cb6ac8acfabb80
Laura-boot <marylaura0205@gmail.com>
2023-05-11 16:41:12 -0300

    [Added footer]

---
+++ react_app/data/script.js
@@ -0,0 +1,19 @@
+const nav = document.querySelector('.navbar')
+fetch('/navbar.html')
+.then(res=>res.text())
+.then(data=>{
+    nav.innerHTML=data
+    const parser = new DOMParser()
+    const doc = parser.parseFromString(data, 'text/html')
+    eval(doc.querySelector('script').textContent)
+})
+
+const footer = document.querySelector('.footer')
+fetch('/footer.html')
+.then(res=>res.text())
+.then(data=>{
+    footer.innerHTML=data
+    const parser = new DOMParser()
+    const doc = parser.parseFromString(data, 'text/html')
+    eval(doc.querySelector('script').textContent)
+})
